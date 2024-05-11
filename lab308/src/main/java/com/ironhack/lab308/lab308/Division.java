37a42560a00ca98870808ffb153c82c6e93e287b
laloosun <lalu.sandu@gmail.com>
2024-05-11 10:47:34 +0100

    fourth draft Laura

--- lab308/src/main/java/com/ironhack/lab308/lab308/Division.java
+++ lab308/src/main/java/com/ironhack/lab308/lab308/Division.java
@@ -9,16 +9,61 @@ public class Division {
 @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String district;
-
     @ManyToOne
     @JoinColumn(name = "president_id")
+
     private Member president; // Representing the president as a Member object
 
     @OneToMany(mappedBy = "division", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Member> members;
+
+    // constructors
+    public Division() {
+        // no-arg constructor
+    }
+
+    // Getters setters
+    public Long getId() {
+        return id;
+
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDistrict() {
+            return district;
+        }
+
+        public void setDistrict(String district) {
+            this.district = district;
+        }
+
+        public Member getPresident() {
+            return president;
+        }
+
+        public void setPresident(Member president) {
+            this.president = president;
+        }
+
+        public List<Member> getMembers() {
+            return members;
+        }
+
+        public void setMembers(List<Member> members) {
+            this.members = members;
+        }    }
 
 
 }
--- lab308/src/main/java/com/ironhack/lab308/lab308/Member.java
+++ lab308/src/main/java/com/ironhack/lab308/lab308/Member.java
@@ -9,14 +9,48 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     @Enumerated(EnumType.STRING)
     private MemberStatus status; // status property
-
     @Temporal(TemporalType.DATE)
     private Date renewalDate;
 
-    // getters setters
+
+    // Constructors
+    public Member() {
+        // No-argument constructor
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRenewalDate() {
+        return renewalDate;
+    }
+
+    public void setRenewalDate(Date renewalDate) {
+        this.renewalDate = renewalDate;
+    }
 }
